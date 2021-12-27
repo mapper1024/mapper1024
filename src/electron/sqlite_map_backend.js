d468b8560bc839a2b5f9e50b08523555000b1ba8
Benjamin Leskey <linewriter1024@gmail.com>
2021-12-27 16:03:47 -0500

    Use better-sqlite3

--- src/electron/sqlite_map_backend.js
+++ src/electron/sqlite_map_backend.js
@@ -1,7 +1,6 @@
 import { MapBackend, Point } from "../../mapper/index.js";
 
-const Database = require('better-sqlite3');
-const sqlite = require("sqlite");
+const Database = require("better-sqlite3");
 
 class SQLiteMapBackend extends MapBackend {
 	constructor(filename) {
@@ -10,99 +9,84 @@ class SQLiteMapBackend extends MapBackend {
 	}
 
 	async load() {
-		this.db = await sqlite.open({
-			filename: this.filename,
-			driver: Database,
-		});
+		this.db = Database(this.filename);
 
-		await this.db.run("CREATE TABLE IF NOT EXISTS entity (entityid INTEGER PRIMARY KEY, type TEXT)");
-		await this.db.run("CREATE TABLE IF NOT EXISTS node (nodeid INT PRIMARY KEY, parentid INT, FOREIGN KEY (nodeid) REFERENCES entity(entityid), FOREIGN KEY (parentid) REFERENCES entity(entityid))");
+		this.db.prepare("CREATE TABLE IF NOT EXISTS entity (entityid INTEGER PRIMARY KEY, type TEXT)").run();
+		this.db.prepare("CREATE TABLE IF NOT EXISTS node (nodeid INT PRIMARY KEY, parentid INT, FOREIGN KEY (nodeid) REFERENCES entity(entityid), FOREIGN KEY (parentid) REFERENCES entity(entityid))").run();
 
-		await Promise.all([
-			/* Each property can be either a
-			 * string
-			 * number (real float)
-			 * point (x, y, and z real float)
-			 */
-			this.db.run("CREATE TABLE IF NOT EXISTS property (entityid INT, property TEXT, v_string TEXT, v_number REAL, x REAL, y REAL, z REAL, PRIMARY KEY (entityid, property), FOREIGN KEY (entityid) REFERENCES entity(entityid))"),
-			this.db.run("CREATE TABLE IF NOT EXISTS neighbor (nodeaid INT, nodebid INT, PRIMARY KEY (nodeaid, nodebid), FOREIGN KEY (nodeaid) REFERENCES node(nodeid) FOREIGN KEY (nodebid) REFERENCES node(nodeid))"),
-		]);
+		/* Each property can be either a
+			* string
+			* number (real float)
+			* point (x, y, and z real float)
+			*/
+		this.db.prepare("CREATE TABLE IF NOT EXISTS property (entityid INT, property TEXT, v_string TEXT, v_number REAL, x REAL, y REAL, z REAL, PRIMARY KEY (entityid, property), FOREIGN KEY (entityid) REFERENCES entity(entityid))").run();
+		this.db.prepare("CREATE TABLE IF NOT EXISTS neighbor (nodeaid INT, nodebid INT, PRIMARY KEY (nodeaid, nodebid), FOREIGN KEY (nodeaid) REFERENCES node(nodeid) FOREIGN KEY (nodebid) REFERENCES node(nodeid))").run();
 
-		[
-			this.s_gpn,
-			this.s_spn,
-			this.s_gpp,
-			this.s_spp,
-			this.s_gps,
-			this.s_sps,
-			this.s_createEntity,
-		] = await Promise.all([
-			this.db.prepare("SELECT v_number FROM property WHERE entityid = $entityId AND property = $property"),
-			this.db.prepare("INSERT OR REPLACE INTO property (entityid, property, v_number) VALUES ($entityId, $property, $value)"),
-			this.db.prepare("SELECT x, y, z FROM property WHERE entityid = $entityId AND property = $property"),
-			this.db.prepare("INSERT OR REPLACE INTO property (entityid, property, x, y, z) VALUES ($entityId, $property, $x, $y, $z)"),
-			this.db.prepare("SELECT v_string FROM property WHERE entityid = $entityId AND property = $property"),
-			this.db.prepare("INSERT OR REPLACE INTO property (entityid, property, v_string) VALUES ($entityId, $property, $value)"),
-			this.db.prepare("INSERT INTO entity (type) VALUES ($type)"),
-		]);
+		this.s_gpn = this.db.prepare("SELECT v_number FROM property WHERE entityid = $entityId AND property = $property");
+		this.s_spn = this.db.prepare("INSERT OR REPLACE INTO property (entityid, property, v_number) VALUES ($entityId, $property, $value)");
+		this.s_gpp = this.db.prepare("SELECT x, y, z FROM property WHERE entityid = $entityId AND property = $property");
+		this.s_spp = this.db.prepare("INSERT OR REPLACE INTO property (entityid, property, x, y, z) VALUES ($entityId, $property, $x, $y, $z)");
+		this.s_gps = this.db.prepare("SELECT v_string FROM property WHERE entityid = $entityId AND property = $property");
+		this.s_sps = this.db.prepare("INSERT OR REPLACE INTO property (entityid, property, v_string) VALUES ($entityId, $property, $value)");
+		this.s_createEntity = this.db.prepare("INSERT INTO entity (type) VALUES ($type)");
 
-		let globalEntityId = await this.db.get("SELECT entityid FROM entity WHERE type = 'global'");
+		let globalEntityIdRow = this.db.prepare("SELECT entityid FROM entity WHERE type = 'global'").get();
 
-		if(globalEntityId === undefined) {
+		if(globalEntityIdRow === undefined) {
 			this.global = await this.createEntity("global");
 		}
 		else {
-			this.global = this.getEntityRef(globalEntityId);
+			this.global = this.getEntityRef(globalEntityIdRow.entityid);
 		}
 	}
 
 	async createEntity(type) {
-		return this.getEntityRef((await this.s_createEntity.run({$type: type})).lastID);
+		return this.getEntityRef(this.s_createEntity.run({type: type}).lastInsertRowid);
 	}
 
 	async getPNumber(entityId, propertyName) {
 		return (await this.s_gpn.get({
-			$entityId: entityId,
-			$property: propertyName,
+			entityId: entityId,
+			property: propertyName,
 		})).v_number;
 	}
 
 	async setPNumber(entityId, propertyName, value) {
 		return this.s_spn.run({
-			$entityId: entityId,
-			$property: propertyName,
-			$value: value,
+			entityId: entityId,
+			property: propertyName,
+			value: value,
 		});
 	}
 
 	async getPPoint(entityId, propertyName) {
 		const row = await this.s_gpp.get({
-			$entityId: entityId,
-			$property: propertyName,
+			entityId: entityId,
+			property: propertyName,
 		});
 		return new Point(row.x, row.y, row.z);
 	}
 
 	async setPPoint(entityId, propertyName, point) {
 		return this.s_spp.run({
-			$entityId: entityId,
-			$property: propertyName,
-			$x: point.x,
-			$y: point.y,
-			$z: point.z,
+			entityId: entityId,
+			property: propertyName,
+			x: point.x,
+			y: point.y,
+			z: point.z,
 		});
 	}
 
 	async getPString(entityId, propertyName) {
 		return (await this.s_gps.get({
-			$entityId: entityId,
+			entityId: entityId,
 			$property: propertyName,
 		})).v_string;
 	}
 
 	async setPString(entityId, propertyName, value) {
 		return this.s_sps.run({
-			$entityId: entityId,
+			entityId: entityId,
 			$property: propertyName,
 			$value: value,
 		});
