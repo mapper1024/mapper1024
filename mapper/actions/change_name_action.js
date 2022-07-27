bfe394bb90654c91a8588397a9a8a71ef2731339
Benjamin Leskey <linewriter1024@gmail.com>
2022-07-27 12:17:24 -0400

    Fix new JS syntax so tests work.

--- mapper/actions/change_name_action.js
+++ mapper/actions/change_name_action.js
@@ -2,7 +2,7 @@ import { Action } from "./action.js";
 
 class ChangeNameAction extends Action {
 	async perform() {
-		const oldName = (await this.options.nodeRef.getPString("name")) ?? "";
+		const oldName = (await this.options.nodeRef.getPString("name")) || "";
 		await this.options.nodeRef.setPString("name", this.options.name);
 		await this.context.mapper.hooks.call("updateNode", this.options.nodeRef);
 		return new ChangeNameAction(this.context, {nodeRef: this.options.nodeRef, name: oldName});
