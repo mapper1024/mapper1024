118ed910b007f9e46297988379624d31c1c6e406
Benjamin Leskey <linewriter1024@gmail.com>
2023-01-12 16:56:29 -0500

    Bigger default window

--- src/electron/main.js
+++ src/electron/main.js
@@ -8,8 +8,8 @@ remoteMain.initialize();
 
 const createWindow = () => {
 	const win = new BrowserWindow({
-		width: 800,
-		height: 600,
+		width: 1024,
+		height: 768,
 		autoHideMenuBar: true,
 		webPreferences: {
 			preload: path.join(__dirname, "preload.js"),
